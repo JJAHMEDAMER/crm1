673801346ba99ee552393618bb1485fc90cd1c72
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-08-22 23:16:26 +0200

    commit

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ixnv3i)7zsly#mq&vz+hbzxn+pl(3x@g+$6r4(lp!ah!f+oq_b'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
